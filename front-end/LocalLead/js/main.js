a37f6591fcbf59b221a5b795cc51e5390ad07ef1
Arthur Martins <arthurmartinsh6@gmail.com>
2026-06-30 10:50:39 -0300

    Add files via upload

---
+++ front-end/LocalLead/js/main.js
@@ -0,0 +1,9 @@
+import { initSolucao } from "./solucao.js";
+import { initMenu } from "./menu.js";
+import { initFaq } from "./faq.js";
+import { initContato } from "./contato.js";
+
+initMenu();
+initFaq();
+initSolucao();
+initContato();
